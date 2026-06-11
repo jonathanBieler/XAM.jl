c408d2309c8afec3de6a0531ea592c08eb856448
Jonathan Bieler <jbieler@sophiagenetics.com>
2026-06-11 15:05:24 +0000

    added test for quality and sequence methods

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ using XAM
 import BioAlignments: Alignment, AlignmentAnchor, OP_START, OP_MATCH, OP_DELETE
 import BGZFLib: BGZFReader, BGZFWriter
 import BioGenerics.Exceptions: MissingFieldException
-import BioSequences: @dna_str, @aa_str
+import BioSequences: @dna_str, @aa_str, LongDNA
 
 
 # Generate a random range within `range`.
